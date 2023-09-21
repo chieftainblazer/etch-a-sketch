759dda7157a2182b3e2820abfe23d5cecef91c27
chieftainblazer <110900442+chieftainblazer@users.noreply.github.com>
2023-09-21 12:07:18 -0700

    Completed the functionality of custom rows and columns. All done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function createGrid() {
+function createGrid(numberOfRows = 4, numberOfColumns = 4) {
     let gridSubContainer = document.createElement('div');
     gridSubContainer.classList.add('grid-subcontainer');
 
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= numberOfRows * numberOfColumns; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-item');
-        if (i % 4 == 0)  {
+        if (i % numberOfColumns == 0)  {
             gridSubContainer.appendChild(gridElement);
             gridContainer.appendChild(gridSubContainer);
             gridSubContainer = document.createElement('div');
@@ -36,6 +36,26 @@ function getRandomColor() {
     return `rgb(${red}, ${green}, ${blue})`;
 }
 
+function getInputFromUser() {
+    let numberOfRows, numberOfColumns;
+    while (true) {
+        alert("Input the number of rows and columns. Please, only input numbers" +
+                " and the maximum number is 100.");
+        numberOfRows = Number(prompt("Number of Rows: "));
+        numberOfColumns = Number(prompt("Number of Columns: "));
+        if (!isNaN(numberOfRows) && !isNaN(numberOfColumns) && 
+            numberOfRows <= 100 && numberOfColumns <= 100) {
+            break;
+        }
+    }
+    gridContainer.innerHTML = null;
+    createGrid(numberOfRows, numberOfColumns);
+    addHoverEffect();
+}
+
 const gridContainer = document.querySelector('.grid');
+const button = document.querySelector('button');
+
 createGrid();
 addHoverEffect();
+button.addEventListener('click', getInputFromUser);
